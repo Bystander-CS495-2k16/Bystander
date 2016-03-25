265171399efa6a17c08aa3f278773a1b1b780573
appleowner <alpuente@crimson.ua.edu>
2016-03-25 10:29:28 -0500

    making broadcast and live stream objects

--- app/src/main/java/com/cs495/bystander/MainActivity.java
+++ app/src/main/java/com/cs495/bystander/MainActivity.java
@@ -1,13 +1,287 @@
 package com.cs495.bystander;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.provider.MediaStore.Files.FileColumns;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.common.AccountPicker;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.api.services.youtube.YouTube;
+/*import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Activity;*/
+import android.content.Context;
+import android.widget.Toast;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import android.app.Dialog;
+import android.accounts.AccountManager;
+
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+
+import com.google.api.services.youtube.model.LiveBroadcastSnippet;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.youtube.model.LiveBroadcastStatus;
+import com.google.api.services.youtube.model.LiveBroadcast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private Uri fileUri;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+    private static String TOKEN;
+    private static YouTube youtube;
+    //String SCOPE = "oauth2:https://www.googleapis.com/auth/youtube.upload";
+    //String SCOPE  = "oauth2:https://www.googleapis.com/auth/youtube";
+    String SCOPE = "oauth2:https://www.googleapis.com/auth/youtube.force-ssl";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
+
+    public void pickAccount(View view) {
+        String[] accountTypes = new String[]{"com.google"};
+        Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                accountTypes, false, null, null, null, null);
+        startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
+    }
+
+
+    public void takeVideo(View view) {
+
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+        File fileUri = getOutputMediaFile(MEDIA_TYPE_VIDEO); // create a file to save the image
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+
+        // start the image capture Intent
+        startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_CODE_PICK_ACCOUNT) {
+
+            // Receiving a result from the AccountPicker
+            if (resultCode == RESULT_OK) {
+
+                mEmail = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                // With the account name acquired, go get the auth token
+                getUsername();
+            } else if (resultCode == RESULT_CANCELED) {
+
+                // The account picker dialog closed without selecting an account.
+                // Notify users that they must pick an account to proceed.
+                Toast.makeText(this, "Woah pick an account", Toast.LENGTH_SHORT).show();
+            }
+        }
+        //
+        //Handle the result from exceptions
+
+    }
+
+    String mEmail; // Received from newChooseAccountIntent(); passed to getToken()
+
+
+    /**
+     * Attempts to retrieve the username.
+     * If the account is not yet known, invoke the picker. Once the account is known,
+     * start an instance of the AsyncTask to get the auth token and do work with it.
+     */
+    private void getUsername() {
+        for (int i = 0; i < 15; i++) {System.out.println("iiii");}
+
+        if (mEmail == null) {
+            for (int i = 0; i < 15; i++) {System.out.println("jjjj");}
+
+        } else {
+            if (isDeviceOnline(this)) {
+                for (int i = 0; i < 15; i++) {System.out.println("hey");}
+                new GetUsernameTask(MainActivity.this, mEmail, SCOPE).execute();
+            } else {
+                Toast.makeText(this, "Your device is not currently online!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public boolean isDeviceOnline(Context c) {
+        ConnectivityManager cm = (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        if (ni == null) {
+            // There are no active networks.
+            return false;
+        }
+        return ni.isConnected();
+    }
+
+    /**
+     * Create a File for saving an image or video
+     */
+    public static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("MADE VIDEO");
+            System.out.println(mediaFile.toString());
+        }
+
+        return mediaFile;
+    }
+
+    public void makeYouTube(View view) {
+        System.out.println("sksdjkfjjfjf");
+        new MakeBroadcast(TOKEN, this);
+    }
+
+
+    public class GetUsernameTask extends AsyncTask<Void, Void, Void> {
+        Context mActivity; // changed this to context from example code's activity type
+        String mScope;
+        String mUsername;
+
+        GetUsernameTask(Context activity, String name, String scope) {
+            this.mActivity = activity;
+            this.mScope = scope;
+            System.out.println(mScope);
+            this.mUsername = name;
+        }
+
+        /**
+         * Executes the asynchronous job. This runs when you call execute()
+         * on the AsyncTask instance.
+         */
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TOKEN = fetchToken();
+                for (int i = 0; i < 15; i++) {
+                    System.out.println("TOKENANNNNNN " + TOKEN);}
+                /*youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+                    @Override
+                    public void initialize(HttpRequest request) throws IOException {}}).setApplicationName("VideoRecorder").build();
+                        // The fetchToken() method handles Google-specific exceptions,*/
+                // so this indicates something went, new  wrong at a higher level.
+                // TIP: Check for network connectivity before starting the AsyncTask.
+            } catch (IOException e) {
+
+            }
+            return null;
+        }
+
+        /**
+         * Gets an authentication token from Google and handles any
+         * GoogleAuthException that may o
+         * ccur.
+         */
+        protected String fetchToken() throws IOException {
+            try {
+                System.out.println("m activity " +  mUsername + " scope " + mScope);
+                return GoogleAuthUtil.getToken(mActivity, mUsername, mScope);
+            } catch (UserRecoverableAuthException userRecoverableException) {
+                // GooglePlayServices.apk is either old, disabled, or not present
+                // so we need to show the user some UI in the activity to recover.
+                handleException(userRecoverableException);
+            } catch (GoogleAuthException fatalException) {
+                // Some other type of unrecoverable exception has occurred.
+                // Report and log the error as appropriate for your app.
+            }
+            return null;
+        }
+
+        static final int REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR = 1001;
+
+        public void handleException(final Exception e) {
+            // Because this call comes from the AsyncTask, we must ensure that the following
+            // code instead executes on the UI thread.
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (e instanceof GooglePlayServicesAvailabilityException) {
+                        // The Google Play services APK is old, disabled, or not present.
+                        // Show a dialog created by Google Play services that allows
+                        // the user to update the APK
+                        int statusCode = ((GooglePlayServicesAvailabilityException)e)
+                                .getConnectionStatusCode();
+                        Dialog dialog = GooglePlayServicesUtil.getErrorDialog(statusCode,
+                                MainActivity.this,
+                                REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
+                        dialog.show();
+                    } else if (e instanceof UserRecoverableAuthException) {
+                        // Unable to authenticate, such as when the user has not yet granted
+                        // the app access to the account, but the user can fix this.
+                        // Forward the user to an activity in Google Play services.
+                        Intent intent = ((UserRecoverableAuthException)e).getIntent();
+                        startActivityForResult(intent,
+                                REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
+                    }
+                }
+            });
+        }
+
+        /**
+         * This method is a hook for background threads and async tasks that need to
+         * provide the user a response UI when an exception occurs.
+         */
+
+    }
+
+
 }
---
+++ app/src/main/java/com/cs495/bystander/MakeBroadcast.java
@@ -0,0 +1,200 @@
+package com.cs495.bystander;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.LiveBroadcast;
+import com.google.api.services.youtube.model.LiveBroadcastSnippet;
+import com.google.api.services.youtube.model.LiveBroadcastStatus;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import com.google.api.services.youtube.model.LiveStreamSnippet;
+import com.google.api.services.youtube.model.CdnSettings;
+import com.google.api.services.youtube.model.LiveStream;
+
+import android.app.Activity;
+import android.view.View;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+/**
+ * Created by appleowner on 3/10/16.
+ */
+public class MakeBroadcast {
+    String TOKEN;
+    Activity mainAct;
+
+    public MakeBroadcast(String token, Activity activity) {
+        System.out.println("sksdjkfjjfjf");
+        TOKEN = token;
+        System.out.println("toke: " + token);
+        mainAct = activity;
+        new makeBroadcast(token).execute();
+    }
+
+    public void make() {
+
+    }
+
+    public class makeBroadcast extends AsyncTask<Void, Void, Void> {
+        Context mActivity; // changed this to context from example code's activity type
+        String mScope;
+        String mUsername;
+
+        makeBroadcast(String token) {
+            System.out.println("heyyyyy");
+            this.mUsername = token;
+        }
+
+        /**
+         * Executes the asynchronous job. This runs when you call execute()
+         * on the AsyncTask instance.
+         */
+        @Override
+        protected Void doInBackground(Void... params) {
+            //System.out.println("DLKJSLKJFLKJFDLKJSDLKJSFLKJ");
+            makeYouTube();
+            return null;
+        }
+
+        public void makeYouTube() {
+            System.out.println("HEODLK");
+            HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+            JacksonFactory JSON_FACTORY = new JacksonFactory();
+            GoogleCredential credential = new GoogleCredential.Builder()
+                    .setJsonFactory(JSON_FACTORY)
+                    .setTransport(HTTP_TRANSPORT).build();
+            System.out.println("as;kjf;askdjf;sakjf;laskj;sadlkjf");
+
+            credential.setAccessToken(TOKEN);
+            System.out.println("sdafja;ksfjd;askj");
+
+            YouTube youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                    .setApplicationName("com.cs495.bystander").build();
+            System.out.println("boooooop");
+
+            String title = "new video";
+            LiveBroadcastSnippet broadcastSnippet = new LiveBroadcastSnippet();
+            broadcastSnippet.setTitle(title);
+
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
+            Calendar cal = Calendar.getInstance();
+            System.out.println(dateFormat.format(cal.getTime())); //2014/08/06 16:00:22
+            broadcastSnippet.setScheduledStartTime(new DateTime(cal.getTime()));
+
+            //broadcastSnippet.setScheduledEndTime(new DateTime("2024-01-31T00:00:00.000Z"));
+            System.out.println("after snippet");
+            LiveBroadcastStatus status = new LiveBroadcastStatus();
+            status.setPrivacyStatus("private");
+            System.out.println("after status ");
+            LiveBroadcast broadcast = new LiveBroadcast();
+            System.out.println("after broadcast");
+            broadcast.setKind("youtube#liveBroadcast");
+            broadcast.setSnippet(broadcastSnippet);
+            broadcast.setStatus(status);
+
+            // Construct and execute the API request to insert the broadcast.
+            try {
+                YouTube.LiveBroadcasts.Insert liveBroadcastInsert =
+                        youtube.liveBroadcasts().insert("snippet,status", broadcast);
+                System.out.println("trying to insert");
+                LiveBroadcast returnedBroadcast = liveBroadcastInsert.execute();
+                // Print information from the API response.
+                System.out.println("\n================== Returned Broadcast ==================\n");
+                System.out.println("  - Id: " + returnedBroadcast.getId());
+                System.out.println("  - Title: " + returnedBroadcast.getSnippet().getTitle());
+                System.out.println("  - Description: " + returnedBroadcast.getSnippet().getDescription());
+                System.out.println("  - Published At: " + returnedBroadcast.getSnippet().getPublishedAt());
+                System.out.println(
+                        "  - Scheduled Start Time: " + returnedBroadcast.getSnippet().getScheduledStartTime());
+                System.out.println(
+                        "  - Scheduled End Time: " + returnedBroadcast.getSnippet().getScheduledEndTime());
+
+                LiveStreamSnippet streamSnippet = new LiveStreamSnippet();
+                streamSnippet.setTitle(title);
+                CdnSettings cdnSettings = new CdnSettings();
+                cdnSettings.setFormat("1080p");
+                cdnSettings.setIngestionType("rtmp");
+
+
+                LiveStream stream = new LiveStream();
+                stream.setKind("youtube#liveStream");
+                stream.setSnippet(streamSnippet);
+                stream.setCdn(cdnSettings);
+
+
+
+                // Construct and execute the API request to insert the stream.
+                YouTube.LiveStreams.Insert liveStreamInsert =
+                        youtube.liveStreams().insert("snippet,cdn", stream);
+                LiveStream returnedStream = liveStreamInsert.execute();
+                System.out.println("address " + returnedStream.getCdn().getIngestionInfo().getIngestionAddress());
+
+                // Print information from the API response.
+                System.out.println("\n================== Returned Stream ==================\n");
+                System.out.println("  - Id: " + returnedStream.getId());
+                System.out.println("  - Title: " + returnedStream.getSnippet().getTitle());
+                System.out.println("  - Description: " + returnedStream.getSnippet().getDescription());
+                System.out.println("  - Published At: " + returnedStream.getSnippet().getPublishedAt());
+
+                // Construct and execute a request to bind the new broadcast
+                // and stream.
+                YouTube.LiveBroadcasts.Bind liveBroadcastBind =
+                        youtube.liveBroadcasts().bind(returnedBroadcast.getId(), "id,contentDetails");
+                liveBroadcastBind.setStreamId(returnedStream.getId());
+                returnedBroadcast = liveBroadcastBind.execute();
+
+                // Print information from the API response.
+                System.out.println("\n================== Returned Bound Broadcast ==================\n");
+                System.out.println("  - Broadcast Id: " + returnedBroadcast.getId());
+                System.out.println(
+                        "  - Bound Stream Id: " + returnedBroadcast.getContentDetails().getBoundStreamId());
+
+
+
+
+            } catch (Exception e) {
+                handleException(e);
+            }
+
+        }
+
+
+        public void handleException(final Exception e) {
+            // Because this call comes from the AsyncTask, we must ensure that the following
+            // code instead executes on the UI thread.
+            mainAct.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (e instanceof Exception) {
+                        // The Google Play services APK is old, disabled, or not present.
+                        // Show a dialog created by Google Play services that allows
+                        // the user to update the APK
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        /**
+         * This method is a hook for background threads and async tasks 
+         *that need to
+         * provide the user a response UI when an exception occurs.
+         */
+
+    }
+}
